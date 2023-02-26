b3581570a694c3a089e1a4f55b333b32bea59c08
Cyberaurora <22611640+cyberaurora@users.noreply.github.com>
2023-02-27 00:11:48 +0800

    update copyright year

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 UINB Technologies Pte. Ltd.
+// Copyright 2021-2023 UINB Technologies Pte. Ltd.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
