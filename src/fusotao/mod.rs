715541fe0a6bc8c120b28f6edfbeb7994e422098
stewart <16131485+StewartYe@users.noreply.github.com>
2023-03-02 22:27:05 +0800

    fmt code

--- src/fusotao/mod.rs
+++ src/fusotao/mod.rs
@@ -38,8 +38,7 @@ pub type BlockNumber = u32;
 pub type FusoHeader = sp_runtime::generic::Header<BlockNumber, FusoHash>;
 pub type FusoExtrinsic = sp_runtime::OpaqueExtrinsic;
 pub type FusoBlock = sp_runtime::generic::Block<FusoHeader, FusoExtrinsic>;
-pub type FusoApi =
-    sub_api::Api<Sr25519Key, sub_api::rpc::WsRpcClient>;
+pub type FusoApi = sub_api::Api<Sr25519Key, sub_api::rpc::WsRpcClient>;
 
 const ONE_ONCHAIN: u128 = 1_000_000_000_000_000_000;
 const MILL: u32 = 1_000_000;
