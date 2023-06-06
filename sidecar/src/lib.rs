0abee7908a51cbdab911e51743ff1e0f48c80b94
stewart <16131485+StewartYe@users.noreply.github.com>
2023-06-06 20:43:39 +0800

    fix derive sub account

--- sidecar/src/lib.rs
+++ sidecar/src/lib.rs
@@ -31,7 +31,8 @@ pub use sp_core::ecdsa::{Pair as EcdsaPair, Public as EcdsaPublic, Signature as 
 pub use sp_core::sr25519::{
     Pair as Sr25519Pair, Public as Sr25519Public, Signature as Sr25519Signature,
 };
-use sp_core::{crypto::Ss58Codec, Pair};
+use sp_core::{ByteArray, crypto::Ss58Codec, Pair};
+
 
 pub fn hexstr_to_vec(h: impl AsRef<str>) -> anyhow::Result<Vec<u8>> {
     hex::decode(h.as_ref().trim_start_matches("0x")).map_err(|_| anyhow::anyhow!("invalid hex str"))
@@ -98,8 +99,8 @@ pub fn try_into_account(addr: String) -> anyhow::Result<AccountId32> {
     }
 }
 
-pub fn derive_sub_account(user_id: Vec<u8>, bot_id: Vec<u8>, token: u32) -> AccountId32 {
-    let h = (b"-*-#fusotao-proxy#-*-", token, user_id, bot_id)
+pub fn derive_sub_account(user_addr: &AccountId32, bot_addr: &AccountId32, token: u32) -> AccountId32 {
+    let h = (b"-*-#fusotao-proxy#-*-", token, user_addr, bot_addr)
         .using_encoded(sp_core::hashing::blake2_256);
     Decode::decode(&mut h.as_ref()).expect("32 bytes; qed")
 }
@@ -108,4 +109,20 @@ pub fn to_mapping_address(address: Vec<u8>) -> AccountId32 {
     let h = (b"-*-#fusotao#-*-", LEGACY_MAPPING_CODE, address)
         .using_encoded(sp_core::hashing::blake2_256);
     Decode::decode(&mut h.as_ref()).expect("32 bytes; qed")
+}
+
+
+#[test]
+fn test_derive_sub_account() {
+    use sp_keyring::AccountKeyring;
+    use std::str::FromStr;
+    let alice = AccountKeyring::Alice.to_account_id();
+    let bot = AccountKeyring::Ferdie.to_account_id();
+    let r = derive_sub_account(&alice, &bot, 1u32);
+    assert_eq!(
+        r,
+        AccountId32::from_str("0x768cff70bf523090fa1d09494cda1d4686361d1bc99129db3d67fe8b57649b7f")
+            .unwrap()
+    );
+
 }
